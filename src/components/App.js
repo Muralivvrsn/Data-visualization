6b5e1e66eff5eca1c4022e42105ecbe0d6e8e71e
Murali <muralivvrsn75683@gmail.com>
2023-03-30 20:36:18 +0530

    changed the path

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,25 @@ import "../App.css";
 import TopHeader from "../components/StaticFiles/TopHeader";
 import LeftHeader from "../components/StaticFiles/LeftHeader";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DashBoard from "./Dashboard";
+import Dashboard from "./Dashboard";
 import DataTables from "./DataTables";
 import MarketPlaces from "./MarketPlaces";
 import Profile from "./Profile";
 import Signin from "./Signin";
-import React,{useState} from 'react';
+import React, { useState } from "react";
 function App() {
-  const [text,setText] = useState("Main Dashboard")
-  function func(data){
+  const [text, setText] = useState("Main Dashboard");
+  function func(data) {
     setText(data);
     console.log(text);
   }
   return (
     <BrowserRouter>
       <div className="App">
-        <TopHeader text={text}/>
-        <LeftHeader text={func}/>
+        <TopHeader text={text} />
+        <LeftHeader text={func} />
         <Routes>
-          <Route path="/" element={<DashBoard />}></Route>
+          <Route path="/" element={<Dashboard />}></Route>
           <Route path="/marketplace" exact element={<MarketPlaces />}></Route>
           <Route path="/datatables" exact element={<DataTables />}></Route>
           <Route path="/profile" exact element={<Profile />}></Route>
